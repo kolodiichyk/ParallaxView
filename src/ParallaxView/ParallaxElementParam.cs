c8c42ae4fa555c73e639983e27b7d195245cc8d0
Andrii Kolodiichyk <kolodiychyk@gmail.com>
2025-03-12 21:45:04 +0200

    Added Zoom functionality

--- src/ParallaxView/ParallaxElementParam.cs
+++ src/ParallaxView/ParallaxElementParam.cs
@@ -5,4 +5,6 @@ internal record ParallaxElementParam
     public double Speed { get; set; }
     public double Y { get; set; }
     public double StickOnY { get; set; }
+    public bool IsZoomed { get; set; }
+    public double ZoomScale { get; set; }
 }
